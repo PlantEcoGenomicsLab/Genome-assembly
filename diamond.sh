dcd78448c481ac12aa306ef37b9c65c4104b6780
PlantEcoGenomicsLab <dalgrandelab@gmail.com>
2024-10-22 11:45:01 +0200

    Update diamond.sh

--- diamond.sh
+++ diamond.sh
@@ -2,25 +2,31 @@
 
 #retrieve the taxon IDs and sequence IDs for the mapping file
 #if you are working in Homelander this step is not necessary as the sprot.fasta.taxlist.gz is already present
-perl -lane 'if(/^>sp\|(\w+)\|\w+\s.*OX=(\d+)/){print "$1 $2"}' uniprot_sprot.fasta | gzip > uniprot_sprot.taxlist.gz
+# Create the header for the taxonomic mapping file
+echo -e "accession\taccession.version\ttaxid\tgi" > swissprot/swissprot.taxid_map
+
+# extract accession and taxid from Swiss-Prot FASTA headers
+cat swissprot/uniprot_sprot.fasta | \
+grep "^>" | \
+awk -F'[| ]' '/OX=/ {for(i=1;i<=NF;i++) if($i ~ /OX=/) {split($i, a, "="); print $2 "\t" $2 "\t" a[2] "\t0"}}' >> swissprot/swissprot.taxid_map
 
 #create the diamond database using swiss-prot database and mapping file
 module load diamond
 
 #if you are working in Homelander this step is not necessary as the uniprot_sprot.taxids.dmnd is already present
-diamond makedb 
-        --in /home/apasinato/databases/uniprot_sprot.fasta \
-        --taxonmap /home/apasinato/databases/sprot.fasta.taxlist.gz \
-        --taxonnodes ../taxdump/nodes.dmp \
-        -d uniprot_sprot.taxids.dmnd
+/home/gsingh/tools/diamond/diamond makedb \
+        --in /home/gsingh/campanula_saxifraga/swissprot/uniprot_sprot.fasta \
+        --taxonmap /home/gsingh/campanula_saxifraga/swissprot/swissprot.taxid_map \
+        --taxonnodes /home/gsingh/tools/taxdump/nodes.dmp \
+        --db swissprot.taxids.dmnd &
 
 #run diamond on the assembly
-diamond blastx 
-        --query ASSEMBLY_NAME.fasta \
-        --db /path/to/uniprot_sprot.taxids.dmnd \
-        --outfmt 6 qseqid staxids bitscore qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore \
+/home/gsingh/tools/diamond/diamond blastx \
+        --query  CZoysii_purgedups2.asm.bp.hap1.p_ctg.fasta \
+        --db swissprot.taxids.dmnd \
+        --outfmt 6 qseqid staxids bitscore qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue \
         --sensitive \
         --max-target-seqs 5 \
         --evalue 1e-25 \
         --threads 16 \
-        > ASSEMBLY_NAME.diamond.blastx.out
+        > c_zoysii_hap1.diamond.out &
