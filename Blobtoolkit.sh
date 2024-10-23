265bf0f4a993b469704c84e005775114495b7b06
PlantEcoGenomicsLab <dalgrandelab@gmail.com>
2024-10-23 17:32:34 +0200

    Update Blobtoolkit.sh

--- Blobtoolkit.sh
+++ Blobtoolkit.sh
@@ -1,11 +1,8 @@
-#SBATCH --job-name=blobtoolkit
-#SBATCH --error %x-%j.err
-#SBATCH --output %x-%j.out
-#SBATCH --partition=cpu
-#SBATCH --mem=350G
-#SBATCH --cpus-per-task=40
+#protip: if you are running blobtoolkit form homelander first activate conda btk environment
+conda activate btk
 
 module load blobtoolkit
+blobtools create --fasta CZoysii_purgedups2.asm.bp.hap1.p_ctg.fasta &
 
 #adding coverage files
 blobtools add --cov aln_CZoysii_hap1.sorted.bam BlobDir_Czoysii_assembly &
@@ -13,5 +10,5 @@ blobtools add --cov aln_CZoysii_hap1.sorted.bam BlobDir_Czoysii_assembly &
 #adding busco
 blobtools add --busco busco_full_table_Czoysii_hap1.tsv BlobDir_Czoysii_assembly &
 
-#adding blastn
+#adding diamond
 blobtools add --hits c_zoysii_hap1.diamond.out --taxdump /cluster/home/fdalgrande/campanula_rhizobotria/TBG_data/TBG_4752/blobtools/taxdump BlobDir_Czoysii_assembly &
