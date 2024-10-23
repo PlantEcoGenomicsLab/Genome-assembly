0b619261e4c6deda99f687fe8490d426017e4032
PlantEcoGenomicsLab <dalgrandelab@gmail.com>
2024-10-23 18:19:44 +0200

    Create blobtoolkit_filter.sh

---
+++ blobtoolkit_filter.sh
@@ -0,0 +1,8 @@
+blobtools filter \
+     --param length--Min=1000 \
+     --param bestsumorder_phylum--Inv=Streptophyta \
+     --fasta ~/BTK_TUTORIAL/FILES/ASSEMBLY_NAME.fasta \
+     --out BlobDir_czoysii_filtered
+     --summary STDOUT \
+     --table
+     ~ BlobDir_c_zoysii
