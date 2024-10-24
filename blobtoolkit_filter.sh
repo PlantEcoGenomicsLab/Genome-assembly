e0db710aae4c823d074a8b5facb794fc583b1ca5
PlantEcoGenomicsLab <dalgrandelab@gmail.com>
2024-10-24 14:46:11 +0200

    Update blobtoolkit_filter.sh

--- blobtoolkit_filter.sh
+++ blobtoolkit_filter.sh
@@ -1,8 +1,6 @@
 blobtools filter \
-     --param length--Min=1000 \
      --param bestsumorder_phylum--Inv=Streptophyta \
-     --fasta ~/BTK_TUTORIAL/FILES/ASSEMBLY_NAME.fasta \
-     --out BlobDir_czoysii_filtered
-     --summary STDOUT \
-     --table
-     ~ BlobDir_c_zoysii
+     --out c_zoysii_hap1_filtered.blobtools \
+     --fasta CZoysii_purgedups2.asm.bp.hap1.p_ctg.fasta \
+     --summary summary.txt \
+     --table ~ BlobDir_Czoysii_assembly
